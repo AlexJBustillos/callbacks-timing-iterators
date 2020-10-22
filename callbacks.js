323104a0fa62af87722b7d27e4dd7ae8260e9b37
alexjbustillos <alexjbustillos@gmail.com>
2020-10-22 10:02:52 -0400

    [Hello]function work

---
+++ callbacks.js
@@ -0,0 +1,36 @@
+function sayHello(name) {
+    let msg = 'Hello ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('SEI 1019'));
+
+function sayHello(name) {
+    let msg = 'Hello ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('Alex'));
+
+function sayHello(name) {
+    let msg = 'Who is ' + name + '?';
+    return msg; 
+}
+
+console.log(sayHello('Lebron James'));
+
+function sayHello(name) {
+    let msg = 'One more ' + name + ' please!!';
+    return msg;
+}
+
+console.log(sayHello('Javascript'));
+
+function sayHello(name) {
+    let msg = 'Do it again ' + name + ' but why?!?';
+    return msg
+}
+
+console.log(sayHello('My guy'));
+
+
